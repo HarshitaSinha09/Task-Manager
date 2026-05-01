2e67b4f15488ac2051919191f40557d594ca4327
Harshita Sinha <harshitassinha04@gmail.com>
2026-05-01 23:13:01 +0530

    fix cors final working

--- Backend/src/main/java/com/example/Backend/config/CorsConfig.java
+++ Backend/src/main/java/com/example/Backend/config/CorsConfig.java
@@ -14,12 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "http://localhost:5174"
-                        )
+                        .allowedOriginPatterns("*")   // 🔥 IMPORTANT (prod + local both)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
